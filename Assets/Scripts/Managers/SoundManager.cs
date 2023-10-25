1e668aaea2eeea2f0451440726f14d51a0d6dc23
jojo1209 <78849419+jojo1209@users.noreply.github.com>
2023-10-25 14:36:06 +0200

    SoundManager

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public List<AudioClip> clipList = new List<AudioClip>();
+
+    public List<AudioSource> audioSources = new List<AudioSource>();
+
+
+
+
+
+}
