ca46f7981f5ee55af5138f5f4125da57d4f83a27
Kiryonn <60699390+Kiryonn@users.noreply.github.com>
2023-10-25 16:07:33 +0200

    forgot static last commit

--- Assets/Scripts/Managers/ScenesManager.cs
+++ Assets/Scripts/Managers/ScenesManager.cs
@@ -29,7 +29,7 @@ public class ScenesManager : MonoBehaviour
     {
         SceneManager.LoadScene("GameScene");
     }
-    public void Chapter2()
+    public static void Chapter2()
     {
         SceneManager.LoadScene("GameScene");
     }
