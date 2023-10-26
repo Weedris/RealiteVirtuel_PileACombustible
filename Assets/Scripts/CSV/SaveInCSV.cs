adf138e58f2adbb6a7f06a4408efe838c858751e
DJVince81 <39565195+DJVince81@users.noreply.github.com>
2023-10-26 18:16:03 +0200

    Update SaveInCSV
    - Others OS location

--- Assets/Scripts/CSV/SaveInCSV.cs
+++ Assets/Scripts/CSV/SaveInCSV.cs
@@ -21,13 +21,9 @@ public class SaveInCSV
         {
             saveFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
         }
-        else if (osType.Contains("Android"))
+        else
         {
             saveFolder = "/storage/emulated/0/Documents/";
-        }
-        else if (osType.Contains("Mac"))
-        {
-            saveFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "Documents");
         }
     }
 
@@ -37,29 +33,21 @@ public class SaveInCSV
     }
 
     public void save()
-    {
-        if (saveFolder == null)
-        {
-            saveFolder = UnityEngine.Application.persistentDataPath;
-        }
-        
+    {   
         if (!Directory.Exists(saveFolder))
         {
             Directory.CreateDirectory(saveFolder);
         }
-        UnityEngine.Debug.Log(saveFolder);
         string filePath = Path.Combine(saveFolder, "CSV_PAC");
         if (!Directory.Exists(filePath))
         {
             Directory.CreateDirectory(filePath);
         }
-        UnityEngine.Debug.Log(filePath);
         StringBuilder fileBuilder = new();
         fileBuilder.Append("Save_")
                    .Append(DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"))
                    .Append("_data.csv");
         filePath = Path.Combine(filePath, fileBuilder.ToString());
-        UnityEngine.Debug.Log(filePath);
         File.WriteAllText(filePath, stringBuilder.ToString());
     }
 }
