02405e8180f54d8a44a0a6e094a32f4df0a1cd2c
DJVince81 <39565195+DJVince81@users.noreply.github.com>
2023-11-08 13:38:19 +0100

    Déplacement de base

---
+++ Assets/KeyboardActionManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardActionManager : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    public float sensitivity = 2f; // Sensibilité de la souris pour la rotation de la caméra
+
+    private float rotationX = 0;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked; // Verrouille le curseur au centre de l'écran
+    }
+
+    void Update()
+    {
+        // Mouvement de la souris pour la rotation de la caméra
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity;
+
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -90, 90); // Limite la rotation verticale
+
+        transform.Rotate(Vector3.up * mouseX);
+        Camera.main.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+
+        // Mouvement du joueur
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput) * moveSpeed * Time.deltaTime;
+        transform.Translate(movement);
+
+        rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
+
+        // Saut
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        if (IsGrounded())
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    bool IsGrounded()
+    {
+        RaycastHit hit;
+        return Physics.Raycast(transform.position, Vector3.down, out hit, 0.1f);
+    }
+}
