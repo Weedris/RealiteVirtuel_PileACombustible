9ff4fd1f7c3cccc9f93fb97d909c081134da0ed4
Kiryonn <60699390+Kiryonn@users.noreply.github.com>
2023-11-09 15:57:51 +0100

    replaced devil spaces with holy tabs

--- Assets/Scripts/PAC_Auxiliaries/ComponentPlacement.cs
+++ Assets/Scripts/PAC_Auxiliaries/ComponentPlacement.cs
@@ -10,31 +10,31 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class ComponentPlacement : MonoBehaviour
 {
-    public static ComponentPlacement Instance;
+	public static ComponentPlacement Instance;
 
-    private void Start()
-        {
-            if (Instance == null)
-            {
-                Instance = this;
-            }
-            else
-            {
-                Destroy(gameObject);
-            }
-        }
-
-    public void CheckComponentPlacement(XRSocketInteractor socket)
+	private void Start()
 	{
-        traceParser.Instance.traceSocket(socket, socket.GetOldestInteractableSelected().transform.name);
-        
-        if (socket.GetOldestInteractableSelected().transform.name == socket.name && socket.name == GameManager.Instance.state.ToString())
+		if (Instance == null)
 		{
-            socket.GetOldestInteractableSelected().transform.GetComponent<Collider>().enabled = false;
+			Instance = this;
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	public void CheckComponentPlacement(XRSocketInteractor socket)
+	{
+		traceParser.Instance.traceSocket(socket, socket.GetOldestInteractableSelected().transform.name);
+		
+		if (socket.GetOldestInteractableSelected().transform.name == socket.name && socket.name == GameManager.Instance.state.ToString())
+		{
+			socket.GetOldestInteractableSelected().transform.GetComponent<Collider>().enabled = false;
 			socket.GetOldestInteractableSelected().transform.tag = "Placed";
 			SoundManager.Instance.PlaySFX(SfxType.GoodAnswer);
-            GameManager.Instance.NextState();
-        }
+			GameManager.Instance.NextState();
+		}
 		else
 		{
 			SoundManager.Instance.PlaySFX(SfxType.BadAnswer);
@@ -43,29 +43,29 @@ public class ComponentPlacement : MonoBehaviour
 
 	public void CheckComponentPlacement(Transform target, Transform objectToPlace)
 	{
-        traceParser.Instance.traceSocket(target.gameObject, objectToPlace.name);
-        if (objectToPlace.transform.name == target.name && target.name == GameManager.Instance.state.ToString() )
+		traceParser.Instance.traceSocket(target.gameObject, objectToPlace.name);
+		if (objectToPlace.transform.name == target.name && target.name == GameManager.Instance.state.ToString() )
 		{
-            Rigidbody rb = objectToPlace.GetComponent<Rigidbody>();
-            rb.useGravity = false;
-            rb.velocity = Vector3.zero;
-            rb.angularVelocity = Vector3.zero;
+			Rigidbody rb = objectToPlace.GetComponent<Rigidbody>();
+			rb.useGravity = false;
+			rb.velocity = Vector3.zero;
+			rb.angularVelocity = Vector3.zero;
 
-            objectToPlace.GetComponent<Collider>().enabled = false;
-            objectToPlace.tag = "Placed";
-            objectToPlace.SetPositionAndRotation(target.position, target.rotation);
+			objectToPlace.GetComponent<Collider>().enabled = false;
+			objectToPlace.tag = "Placed";
+			objectToPlace.SetPositionAndRotation(target.position, target.rotation);
 
-            SoundManager.Instance.PlaySFX(SfxType.GoodAnswer);
-            GameManager.Instance.NextState();
-        }
-        else
-        {
-            SoundManager.Instance.PlaySFX(SfxType.BadAnswer);
-        }
+			SoundManager.Instance.PlaySFX(SfxType.GoodAnswer);
+			GameManager.Instance.NextState();
+		}
+		else
+		{
+			SoundManager.Instance.PlaySFX(SfxType.BadAnswer);
+		}
 	}
 
 
-    public void Caught(GameObject go)
+	public void Caught(GameObject go)
 	{
 		GameManager.Instance.traceParser.traceInApp(go);
 	}
