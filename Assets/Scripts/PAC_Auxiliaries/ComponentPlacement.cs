ce848c7161ee3edc662fd32b20a7cc6d12f111e7
jojo1209 <78849419+jojo1209@users.noreply.github.com>
2023-11-09 15:53:17 +0100

    modif appelle receive

--- Assets/Scripts/PAC_Auxiliaries/ComponentPlacement.cs
+++ Assets/Scripts/PAC_Auxiliaries/ComponentPlacement.cs
@@ -26,24 +26,25 @@ public class ComponentPlacement : MonoBehaviour
 
     public void CheckComponentPlacement(XRSocketInteractor socket)
 	{
-
-		if (socket.GetOldestInteractableSelected().transform.name == socket.name && socket.name == GameManager.Instance.state.ToString())
+        traceParser.Instance.traceSocket(socket, socket.GetOldestInteractableSelected().transform.name);
+        
+        if (socket.GetOldestInteractableSelected().transform.name == socket.name && socket.name == GameManager.Instance.state.ToString())
 		{
-			socket.GetOldestInteractableSelected().transform.GetComponent<Collider>().enabled = false;
+            socket.GetOldestInteractableSelected().transform.GetComponent<Collider>().enabled = false;
 			socket.GetOldestInteractableSelected().transform.tag = "Placed";
 			SoundManager.Instance.PlaySFX(SfxType.GoodAnswer);
             GameManager.Instance.NextState();
         }
 		else
 		{
-			GameManager.Instance.traceParser.traceSocket(socket, socket.GetOldestInteractableSelected().transform.name);
 			SoundManager.Instance.PlaySFX(SfxType.BadAnswer);
 		}
 	}
 
 	public void CheckComponentPlacement(Transform target, Transform objectToPlace)
 	{
-		if (objectToPlace.transform.name == target.name && target.name == GameManager.Instance.state.ToString() )
+        traceParser.Instance.traceSocket(target.gameObject, objectToPlace.name);
+        if (objectToPlace.transform.name == target.name && target.name == GameManager.Instance.state.ToString() )
 		{
             Rigidbody rb = objectToPlace.GetComponent<Rigidbody>();
             rb.useGravity = false;
@@ -59,8 +60,7 @@ public class ComponentPlacement : MonoBehaviour
         }
         else
         {
-			GameManager.Instance.traceParser.traceSocket(target.gameObject, objectToPlace.name);
-			SoundManager.Instance.PlaySFX(SfxType.BadAnswer);
+            SoundManager.Instance.PlaySFX(SfxType.BadAnswer);
         }
 	}
 
