7b92f4bb268fcd0515d57e104a6da282fba6f673
Kiryonn <60699390+Kiryonn@users.noreply.github.com>
2023-12-21 14:05:00 +0100

    Cleaned a bit CarManager

--- Assets/Scripts/Managers/CarManager.cs
+++ Assets/Scripts/Managers/CarManager.cs
@@ -1,150 +1,144 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Text;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Device;
-using UnityEngine.UI;
-using static UnityEngine.Rendering.DebugUI;
 
 public class CarManager : MonoBehaviour
 {
-    private Animator Animator;
-    private int lastPlayedClipHashStart, lastPlayedClipHashEnd;
-    private DateTime lapsTime;
+	private Animator Animator;
+	private int lastPlayedClipHashStart, lastPlayedClipHashEnd;
+	private DateTime lapsTime;
 
-    [SerializeField]
-    private GameObject finalScoreCanvas;
-    [SerializeField]
-    private TextMeshProUGUI finalScore;
-    private DateTime totalTime;
-    private TimeSpan bestTimeLaps;
-    private int totalLaps;
+	[SerializeField] private GameObject finalScoreCanvas;
+	[SerializeField] private TextMeshProUGUI finalScore;
 
-    private const float ZeroTolerance = 0.0001f; // Tolerance for zero
+	private DateTime totalTime;
+	private TimeSpan bestTimeLaps;
+	private int totalLaps;
 
-    void Start()
-    {
-        Animator = GetComponent<Animator>();
-        if (Animator == null) throw new Exception("Animator null!");
-        finalScoreCanvas.SetActive(false);
-        ResetStats();
-        PauseAnimator();
-        InvokeRepeating("UpdateCar", 0.0f, 0.1f);
-    }
+	private const float ZeroTolerance = 0.0001f; // Tolerance for zero
 
-    void Update()
-    {
-        if (AnimatorIsPlaying())
-        {
-            AnimatorStateInfo stateInfo = Animator.GetCurrentAnimatorStateInfo(0);
-            if (stateInfo.normalizedTime <= 0.05f && lastPlayedClipHashStart != stateInfo.shortNameHash)
-            {
-                lastPlayedClipHashStart = stateInfo.shortNameHash;
-                OnAnimationStart();
-            }
-            if (stateInfo.normalizedTime >= 1.0f && lastPlayedClipHashEnd != stateInfo.shortNameHash)
-            {
-                lastPlayedClipHashEnd = stateInfo.shortNameHash;
-                OnAnimationEnd();
-            }
-        }
-    }
+	void Start()
+	{
+		if (!TryGetComponent(out Animator))
+			throw new Exception("Animator null!");
+		finalScoreCanvas.SetActive(false);
+		ResetStats();
+		PauseAnimator();
+		InvokeRepeating(nameof(UpdateCar), 0.0f, 0.1f);
+	}
 
-    private void ResetStats()
-    {
-        totalTime = DateTime.MinValue;
-        bestTimeLaps = TimeSpan.MaxValue;
-        totalLaps = 0;
-    }
+	void Update()
+	{
+		if (AnimatorIsPlaying())
+		{
+			AnimatorStateInfo stateInfo = Animator.GetCurrentAnimatorStateInfo(0);
+			if (stateInfo.normalizedTime <= 0.05f && lastPlayedClipHashStart != stateInfo.shortNameHash)
+			{
+				lastPlayedClipHashStart = stateInfo.shortNameHash;
+				OnAnimationStart();
+			}
+			if (stateInfo.normalizedTime >= 1.0f && lastPlayedClipHashEnd != stateInfo.shortNameHash)
+			{
+				lastPlayedClipHashEnd = stateInfo.shortNameHash;
+				OnAnimationEnd();
+			}
+		}
+	}
 
-    public void ResetCarManager()
-    {
-        ResetStats();
-    }
+	private void ResetStats()
+	{
+		totalTime = DateTime.MinValue;
+		bestTimeLaps = TimeSpan.MaxValue;
+		totalLaps = 0;
+	}
 
-    public void SetHydrogenAndResetIntensity(float hydrogenValue)
-    {
-        GaugeManager.Instance.ResetValues(hydrogenValue);
-    }
+	public void ResetCarManager()
+	{
+		ResetStats();
+	}
 
-    void SetCarSpeed(float speed)
-    {
-        Animator.SetFloat("Speed", speed);
-    }
+	public void SetHydrogenAndResetIntensity(float hydrogenValue)
+	{
+		GaugeManager.Instance.ResetValues(hydrogenValue);
+	}
 
-    bool AnimatorIsPlaying()
-    {
-        return Animator.speed > 0;
-    }
+	void SetCarSpeed(float speed)
+	{
+		Animator.SetFloat("Speed", speed);
+	}
 
-    void PauseAnimator(bool outOfHydrogen = false)
-    {
-        Animator.speed = 0f;
-        if (outOfHydrogen)
-        {
-            AddLapsTime();
-            finalScoreCanvas.SetActive(true);
-            StringBuilder sb = new StringBuilder();
-            sb.AppendLine("Total time : " + FormatTime(totalTime))
-                .AppendLine("Total laps : " + totalLaps)
-                .AppendLine("Best Time Laps : " + FormatTime(bestTimeLaps));
-            finalScore.text = sb.ToString();
-        }
-    }
+	bool AnimatorIsPlaying()
+	{
+		return Animator.speed > 0;
+	}
 
-    internal void ResumeAnimator()
-    {
-        Animator.speed = 1f;
-    }
+	void PauseAnimator(bool outOfHydrogen = false)
+	{
+		Animator.speed = 0f;
+		if (outOfHydrogen)
+		{
+			AddLapsTime();
+			finalScoreCanvas.SetActive(true);
+			StringBuilder sb = new StringBuilder();
+			sb.AppendLine("Total time : " + FormatTime(totalTime))
+				.AppendLine("Total laps : " + totalLaps)
+				.AppendLine("Best Time Laps : " + FormatTime(bestTimeLaps));
+			finalScore.text = sb.ToString();
+		}
+	}
 
-    private void UpdateCar()
-    {
-        if (AnimatorIsPlaying())
-        {
-            float hydrogen = GaugeManager.Instance.Hydrogen;
+	internal void ResumeAnimator()
+	{
+		Animator.speed = 1f;
+	}
 
-            float speedMultiplier = Mathf.Lerp(0.15f, 0.5f, GaugeManager.Instance.Power/3576.04f);
-            float efficiency = GaugeManager.Instance.Efficiency / 100;
+	private void UpdateCar()
+	{
+		if (AnimatorIsPlaying())
+		{
+			float hydrogen = GaugeManager.Instance.Hydrogen;
 
-            float adjustedSpeed = speedMultiplier - (speedMultiplier * Mathf.Min(efficiency, hydrogen));
-            SetCarSpeed(adjustedSpeed);
+			float speedMultiplier = Mathf.Lerp(0.15f, 0.5f, GaugeManager.Instance.Power / 3039.276f);
+			float efficiency = GaugeManager.Instance.Efficiency / 100;
 
-            GaugeManager.Instance.UpdateOutSliders();
+			float adjustedSpeed = speedMultiplier - (speedMultiplier * Mathf.Min(efficiency, hydrogen));
+			SetCarSpeed(adjustedSpeed);
 
-            if (GaugeManager.Instance.Hydrogen < ZeroTolerance) PauseAnimator(true);
-        }
-    }
+			GaugeManager.Instance.UpdateOutSliders();
 
-    void OnAnimationStart()
-    {
-        lapsTime = DateTime.Now;
-    }
+			if (GaugeManager.Instance.Hydrogen < ZeroTolerance) PauseAnimator(true);
+		}
+	}
 
-    void OnAnimationEnd()
-    {
-        AddLapsTime(true);
-        totalLaps++;
-    }
+	void OnAnimationStart()
+	{
+		lapsTime = DateTime.Now;
+	}
 
-    void AddLapsTime(bool lapsFined = false)
-    {
-        TimeSpan elapsedTime = DateTime.Now - lapsTime;
-        if ((elapsedTime < bestTimeLaps) && lapsFined)
-        {
-            bestTimeLaps = elapsedTime;
-        }
-        totalTime += elapsedTime;
-    }
+	void OnAnimationEnd()
+	{
+		AddLapsTime(true);
+		totalLaps++;
+	}
 
-    string FormatTime(TimeSpan time)
-    {
-        return $"{time.Minutes:D2}:{time.Seconds:D2}:{time.Milliseconds:D3}";
-    }
+	void AddLapsTime(bool lapsFined = false)
+	{
+		TimeSpan elapsedTime = DateTime.Now - lapsTime;
+		if ((elapsedTime < bestTimeLaps) && lapsFined)
+		{
+			bestTimeLaps = elapsedTime;
+		}
+		totalTime += elapsedTime;
+	}
 
-    string FormatTime(DateTime time)
-    {
-        return $"{time.Minute:D2}:{time.Second:D2}:{time.Millisecond:D3}";
-    }
+	string FormatTime(TimeSpan time)
+	{
+		return $"{time.Minutes:D2}:{time.Seconds:D2}:{time.Milliseconds:D3}";
+	}
+
+	string FormatTime(DateTime time)
+	{
+		return $"{time.Minute:D2}:{time.Second:D2}:{time.Millisecond:D3}";
+	}
 }
